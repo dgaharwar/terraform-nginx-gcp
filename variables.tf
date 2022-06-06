8f88c20879347c6f3bdb67f963d991bd25f60941
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2022-06-06 11:32:54 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "gcp_private_key" { 
+  type = string 
+} 
+
+variable "gcp_cred" { 
+  type = map 
+} 
+
+locals {
+  credential = merge(var.gcp_cred, {private_key = "${var.gcp_private_key}"}) 
+} 
+
 variable "project_id" {
   description = "Google Cloud Platform (GCP) Project ID."
   type        = string
