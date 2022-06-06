8bccb23d6446d2ed93a82188b039d660d86b404d
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2022-06-06 12:27:38 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.84.0"
+    }
+  }
+}
+
 # Specify the GCP Provider
 provider "google" {
   project = var.project_id
